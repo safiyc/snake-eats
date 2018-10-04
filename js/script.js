a4cfc1ebf4704a62e3c2456f8c17bd271ba730ca
Safiy Cham <csafiy@yahoo.com>
2018-10-03 22:31:51 -0700

    Create keydown presses for arrow keys

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-let canvas = document.getElementById("testCanvas");
-let ctx = canvas.getContext("2d");
+// canvas portion of site:
+let canvas = document.getElementById("canvas");
+let snake = canvas.getContext("2d");      // 'getContext' is an html object, with properties and methods for drawing
 
-ctx.fillStyle = "black";
-ctx.fillRect(20, 20, 10, 10);
+snake.fillStyle = "black";
+snake.fillRect(20, 20, 10, 10);       // x-cord, y-cord, width, height
+
+// arrowkeys press
+document.addEventListener("keydown", keyDown);
+
+let rightArrowPressed = false;
+let leftArrowPressed = false;
+let upArrowPressed = false;
+let downArrowPressed = false;
+
+function keyDown(event) {
+    if(event.keyCode == 39) {
+        rightArrowPressed = true;
+        console.log("right arrow is pressed");
+    } else if(event.keyCode == 37) {
+        leftArrowPressed = true;
+        console.log("left arrow is pressed");
+    } else if(event.keyCode == 38) {
+        upArrowPressed = true;
+        console.log("up arrow is pressed");
+    } else if(event.keyCode == 40) {
+        downArrowPressed = true;
+        console.log("down arrow is pressed");
+    }
+}
+
+
