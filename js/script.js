369a5877fcb342fe10a3230ed088863cf60840f7
Safiy Cham <csafiy@yahoo.com>
2018-10-04 19:54:52 -0700

    Snake continues to move even after arrow key is released

--- js/script.js
+++ js/script.js
@@ -14,33 +14,36 @@ let leftArrowPressed = false;
 let upArrowPressed = false;
 let downArrowPressed = false;
 
+let intervalControl;        // setInterval/clearInterval to prevent snake increases speed at repeated keydowns
+
 function keyDown(event) {
-    if(event.keyCode == 39) {
+    if (event.keyCode == 39) {
         rightArrowPressed = true;
         leftArrowPressed = false;
         upArrowPressed = false;
         downArrowPressed = false;
         console.log("right arrow is pressed");
-    } else if(event.keyCode == 37) {
+    } else if (event.keyCode == 37) {
         rightArrowPressed = false;
         leftArrowPressed = true;
         upArrowPressed = false;
         downArrowPressed = false;
         console.log("left arrow is pressed");
-    } else if(event.keyCode == 38) {
+    } else if (event.keyCode == 38) {
         rightArrowPressed = false;
         leftArrowPressed = false;
         upArrowPressed = true;
         downArrowPressed = false;
         console.log("up arrow is pressed");
-    } else if(event.keyCode == 40) {
+    } else if (event.keyCode == 40) {
         rightArrowPressed = false;
         leftArrowPressed = false;
         upArrowPressed = false;
         downArrowPressed = true;
         console.log("down arrow is pressed");
     }
-
+    
+    clearInterval(intervalControl);     // reset setInterval before running moveSnake()
     moveSnake();
 }
 
@@ -50,20 +53,24 @@ let snakeWidth = 10;
 let snakeHeight = 10;
 
 function moveSnake() {
-
-    snake.clearRect(0, 0, canvas.width, canvas.height);
-    if(rightArrowPressed) {
-        snakeX++;
-        snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
-    } else if (leftArrowPressed) {
-        snakeX--;
-        snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
-    } else if (upArrowPressed) {
-        snakeY--;       // Why 'up' is 'snakeY--'? Logically should be 'snakeY++'?
-        snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
-    } else if (downArrowPressed) {
-        snakeY++;
-        snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
-    }
+    // snake continues to move after arrow key is released
+    intervalControl = setInterval(function () {
+        snake.clearRect(0, 0, canvas.width, canvas.height);
+        if (rightArrowPressed) {
+            snakeX++;
+            snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+        }
+        if (leftArrowPressed) {
+            snakeX--;
+            snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+        }
+        if (upArrowPressed) {
+            snakeY--;       // Why 'up' is 'snakeY--'? Logically should be 'snakeY++'?
+            snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+        }
+        if (downArrowPressed) {
+            snakeY++;
+            snake.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+        }
+    }, 20);
 }
-
